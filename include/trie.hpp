b35b000150ac9fbfa8d2b86788d8981f372bffcf
Otto Seiskari <otto.seiskari@gmail.com>
2015-10-06 21:48:14 +0300

    first version of char trie

--- include/trie.hpp
+++ include/trie.hpp
@@ -3,25 +3,50 @@
 #include <map>
 #include <stdexcept>
 
-using std::string;
+class CharTrie {
+public:
+    std::map<char, CharTrie> children;
 
-template <class Value>
-struct Trie {
+    typedef std::map<char, CharTrie>::iterator iterator;
+    typedef std::map<char, CharTrie>::const_iterator const_iterator;
     
-    std::map<string, Trie> children;
-    Value v;
+    CharTrie(const void *v = NULL) : value(v) {}
 
-    void insert(string key, Value v) {
+    void insert(const char *key, const void *v, bool replace = false) {
+        CharTrie &where = lookup_subtree(key);
+        where.insert_subtree(key, v, replace);
     }
     
-    Value *lookup(string key) {
-        return NULL;
+    const void *lookup(const char *key) {
+        CharTrie &where = lookup_subtree(key);
+        if (key[0] != '\0') return NULL;
+        return where.value;
     }
     
-    Value &get(string key) {
-        Value *v = lookup(key);
-        if (v == NULL)
-            throw std::runtime_error("key not found");
-        return *v;
+private:
+    const void *value;
+
+    CharTrie &lookup_subtree(const char *&key) {
+        
+        if (key[0] == '\0') return *this;
+        iterator itr = children.find(key[0]);
+        if (itr != children.end()) {
+            key += 1;
+            return itr->second.lookup_subtree(key);
+        }
+        return *this;
+    }
+    
+    void insert_subtree(const char *key, const void *new_v, bool replace) {
+        
+        if (key[0] == '\0') {
+            if (!replace && value != NULL)
+                throw std::runtime_error("key already exists in trie");
+            value = new_v;
+            return;
+        }
+        
+        children[key[0]] = CharTrie();
+        children.find(key[0])->second.insert_subtree(key+1, new_v, replace);
     }
 };
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,19 +1,49 @@
 
 #include <trie.hpp>
 #include <assert.h>
+#include <sstream>
+
+std::ostream &trie_structure_json(const CharTrie &trie, std::ostream &os) {
+    os << "{";
+    CharTrie::const_iterator c = trie.children.begin(); 
+    while (c != trie.children.end()) {
+        os << '\"' << c->first << "\":";
+        trie_structure_json(c->second, os);
+        ++c;
+        if (c != trie.children.end()) os << ",";
+    }
+    os << "}";
+    return os;
+}
+
+std::string trie_structure_json(const CharTrie &trie) {
+    std::ostringstream oss;
+    trie_structure_json(trie,oss);
+    return oss.str();
+}
 
 void run_trie_tests() {
-    Trie<int> t;
+    CharTrie t;
     
-    t.insert("foo", 1);
-    t.insert("bar", 2);
-    t.insert("foobar", 3);
+    int one = 1, two = 2, three = 3;
     
-    std::cout << "ss\n";
-    assert(t.lookup("asdf") == NULL);
-    assert(t.get("foo") == 1);
-    assert(t.get("bar") == 2);
-    assert(t.get("foobar") == 3);
+    t.insert("abcd", &one);
+    
+    assert(trie_structure_json(t) == "{\"a\":{\"b\":{\"c\":{\"d\":{}}}}}");
+    assert(t.lookup("foobar") == NULL);
+    assert(t.lookup("abcd") == &one);
+    
+    t.insert("abf", &two);
+    assert(trie_structure_json(t) == "{\"a\":{\"b\":{\"c\":{\"d\":{}},\"f\":{}}}}");
+    assert(t.lookup("abcd") == &one);
+    assert(t.lookup("abf") == &two);
+    assert(t.lookup("ab") == NULL);
+    
+    t.insert("ab", &three);
+    assert(trie_structure_json(t) == "{\"a\":{\"b\":{\"c\":{\"d\":{}},\"f\":{}}}}");
+    assert(t.lookup("abcd") == &one);
+    assert(t.lookup("abf") == &two);
+    assert(t.lookup("ab") == &three);
 }
 
 int main() {
